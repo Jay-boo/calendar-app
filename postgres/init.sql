73af7a46fae17ac06e2b9f6f661b05a140eaf99e
AdrienMarquer22 <adrien.marquer.pro@gmail.com>
2023-01-15 15:49:07 +0100

    ok

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,17 +1,4 @@
--- Drop the table
 
-DROP TABLE IF EXISTS User_table CASCADE;
-DROP TABLE IF EXISTS Calendar CASCADE;
-DROP TABLE IF EXISTS Reminder CASCADE;
-DROP TABLE IF EXISTS User_calendar CASCADE;
-
---DROP SEQUENCE IF EXISTS calendar_id ;
---DROP SEQUENCE IF EXISTS event_id ;
---DROP SEQUENCE IF EXISTS user_id ;
-
---REATE SEQUENCE calendar_id_seq ;
---CREATE SEQUENCE event_id_seq ;
---CREATE SEQUENCE user_id_seq ;
 
 
 -- Create the database
@@ -20,9 +7,27 @@ CREATE DATABASE calendar_app;
 -- Connect to the database
 \c calendar_app;
 
+
+-- Drop the table
+
+DROP TABLE IF EXISTS User_table CASCADE;
+DROP TABLE IF EXISTS Calendar CASCADE;
+DROP TABLE IF EXISTS Reminder CASCADE;
+DROP TABLE IF EXISTS User_calendar CASCADE;
+
+DROP SEQUENCE IF EXISTS calendar_id ;
+DROP SEQUENCE IF EXISTS event_id ;
+DROP SEQUENCE IF EXISTS user_id ;
+
+CREATE SEQUENCE calendar_id_seq;
+CREATE SEQUENCE event_id_seq;
+CREATE SEQUENCE user_id_seq;
+
+
+
 -- Create the User table
 CREATE TABLE User_table (
-    user_id SERIAL PRIMARY KEY,
+    user_id INTEGER PRIMARY KEY,
     username VARCHAR(255),
     password VARCHAR(255)
 );
@@ -30,13 +35,13 @@ CREATE TABLE User_table (
 -- Create the User_calendar table
 CREATE TABLE User_calendar (
     user_id INTEGER REFERENCES User_table(user_id),
-    calendar_id SERIAL PRIMARY KEY
+    calendar_id INTEGER PRIMARY KEY
 );
 
 -- Create the Calendar table
 CREATE TABLE Calendar (
     calendar_id INTEGER REFERENCES User_calendar(calendar_id) ,
-    event_id SERIAL PRIMARY KEY,
+    event_id INTEGER PRIMARY KEY,
     created_at DATE,
     start_date DATE,
     end_date DATE,
