719c7c3af18a43683bcd71b78a1762d7c52a2a9f
Jay-boo <jeanalainpro2@gmail.com>
2023-01-31 18:23:15 +0100

    pattenr calendar done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import getDay from 'date-fns/getDay';
+import parse from 'date-fns/parse';
+import startOfWeek from "date-fns/startOfWeek"
+import format from "date-fns/format";
+import React, { useState } from "react";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+
+const locales = {
+  "en-US": require("date-fns/locale/en-US")
+}
+
+const localizer = dateFnsLocalizer(
+  {
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+  }
+)
+
+
+const events = [
+  {
+    title: "Big meeting",
+    allDay: true,
+    start: new Date(2023, 1, 29),
+    end: new Date(2023, 1, 31)
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+      <Calendar localizer={localizer} events={events} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
     </div>
   );
 }
